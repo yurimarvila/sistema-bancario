99ef015fbe3ca1b695ede6c22bcaaaf4c51db89a
Yuri Marvila <yurimarvilapessoal@gmail.com>
2026-03-02 22:11:35 -0300

    inicial - estrutura base do projeto

---
+++ src/test/java/br/com/banco/sistema_bancario/SistemaBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.banco.sistema_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
