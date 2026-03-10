c45f8b381cc5631b62d281515c976f7374bede01
Yuri Marvila <yurimarvilapessoal@gmail.com>
2026-03-10 09:38:24 -0300

    feat: GlobalExceptionHandler para tratamento de erros

---
+++ src/main/java/br/com/banco/sistema_bancario/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package br.com.banco.sistema_bancario.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.status(400).body(e.getMessage());
+    }
+}
